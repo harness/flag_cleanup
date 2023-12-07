793a0c8460ee8d4b4cc4616bb2c16e5ac056c702
Andrew Hayes <andrew.hayes@harness.io>
2023-12-07 13:20:46 +0000

    maint updates to js example

--- examples/javascript/example.js
+++ examples/javascript/example.js
@@ -1,10 +1,5 @@
-
 // Simple flag cleanup in conditional
-if(isEnabled("STALE_FLAG")) {
-    f1();
-} else {
-    f2();
-}
+f2();
 
 // Assignment cleanup
 var a = doSomething("OTHER_FLAG");
@@ -14,19 +9,14 @@ if(a) {
     f2();
 }
 
-// function cleanup
-function b() {
-    return isToggleDisabled("STALE_FLAG");
-}
-
-if(b() || f1()) {
+if(f1()) {
     f1()
 } else {
     f2()
 }
 
 // Complex cleanup
-var c = isToggleDisabled("STALE_FLAG") ? f1() : doSomething("STALE_FLAG") ? f2() : isEnabled("OTHER_FLAG") ? f3() : f4();
+var c = f2();
 
 function doSomething(flagName) {
     console.log(flagName)
