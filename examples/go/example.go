46e76a3f0176565a7ea1727c377a001c5c2d9773
andrew-hayes <andrew-hayes@users.noreply.github.com>
2024-01-25 19:42:59 +0000

    Removes stale flag and associated code: STALE_FLAG

--- examples/go/example.go
+++ examples/go/example.go
@@ -23,11 +23,8 @@ func main() {
 	}
 	defer func() { client.Close() }()
 
-	if isEnabled("STALE_FLAG") {
-		log.Println("Run true code path")
-	} else {
-		log.Println("Run false code path")
-	}
+	log.Println("Run true code path")
+
 
 	if isEnabled("OTHER_FLAG") {
 		log.Println("Run true code path")
@@ -37,11 +34,8 @@ func main() {
 }
 
 func doSomething() {
-	if isEnabled("STALE_FLAG") {
-		log.Println("Run true code path")
-	} else {
-		log.Println("Run false code path")
-	}
+	log.Println("Run true code path")
+
 }
 
 func isEnabled(flag string) bool {
